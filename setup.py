ede5349ba6887a92996ae85ae0615d8a9a360adf
guidofenix <guido.barbaglia@fao.org>
2014-09-16 16:35:22 +0200

    TRMM plug-in for PGeo.

--- setup.py
+++ setup.py
@@ -4,13 +4,12 @@ from setuptools import find_packages
 
 setup(
     name='PGeoTRMM',
-    version='0.1.0',
+    version='0.1.1',
     author='Simone Murzilli; Guido Barbaglia',
     author_email='geobrickspy@gmail.com',
     packages=find_packages(),
     install_requires=[
-        'pgeo',
-        'flask'
+
     ],
     url='http://pypi.python.org/pypi/PGeoTRMM/',
     license='LICENSE.txt',
